f62f2876ee93384cd2139a4ca323b5bb45861c25
MasRoesdi <mdotrusdi@gmail.com>
2022-09-19 10:13:22 +0700

    Initialize Repo

---
+++ pages/about.js
@@ -0,0 +1,18 @@
+import Head from "next/head";
+
+export default function About() {
+    return (
+        <div>
+            <Head>
+                <title>About Page</title>
+            </Head>
+            <div>
+                <h1>About us, the developers</h1>
+                <ul>
+                    <h3>Muhammad Rusdiyanto</h3>
+                    <h3>Ravly Ananda</h3>
+                </ul>
+            </div>
+        </div>
+    )
+}
